c91e16799948b7fb3b98fd84ecfc4ffc9a9d6a19
Eduardo Jordan <eduardojordan@hotmail.com>
2018-06-01 22:28:27 +0200

    Practica de Firebase, usuario y registro.

---
+++ SuperPro/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  SuperPro
+//
+//  Created by Eduardo on 1/6/18.
+//  Copyright © 2018 Eduardo Jordan Muñoz. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+       
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+     
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+      
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+      
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+
+}
+
